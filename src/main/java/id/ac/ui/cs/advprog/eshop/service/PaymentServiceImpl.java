bece1608b5581f2d9c4e2573c1a62626c3139b4f
Narwhll <davin.fauzaan@gmail.com>
2026-03-10 17:37:02 +0700

    [GREEN] Added PaymentServiceImpl.

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,4 +1,62 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
-public class PaymentServiceImpl {
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        Payment payment = new Payment(order.getId(), method, paymentData);
+        return paymentRepository.add(payment);
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        payment.setPaymentStatus(status);
+
+        paymentRepository.update(payment.getPaymentId(), payment);
+
+        Order order = orderRepository.findById(payment.getPaymentId());
+
+        if (order != null) {
+            if (PaymentStatus.SUCCESS.getValue().equals(status)) {
+                order.setStatus(OrderStatus.SUCCESS.getValue());
+            } else if (PaymentStatus.REJECTED.getValue().equals(status)) {
+                order.setStatus(OrderStatus.FAILED.getValue());
+            }
+            orderRepository.save(order);
+        } else {
+            throw new NoSuchElementException("Order tidak ditemukan untuk Payment ID: " + payment.getPaymentId());
+        }
+
+        return payment;
+    }
+
+    @Override
+    public Payment getPayment(String paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
 }
